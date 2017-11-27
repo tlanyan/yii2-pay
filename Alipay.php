e9fc51eca47736b2966993fe30d0ef13eda1fc59
U-tlanyan-PC\tlanyan <tlanyan@hotmail.com>
2017-11-27 16:28:46 +0800

    新增支付宝网页版支付

--- Alipay.php
+++ Alipay.php
@@ -13,6 +13,7 @@ use Yii;
 use AopClient;
 use yii\base\Object;
 use AlipayTradeAppPayRequest;
+use AlipayTradeWapPayRequest;
 
 require_once (__DIR__ . '/sdks/alipay/AopSdk.php');
 
@@ -62,49 +63,94 @@ class Alipay extends Object
 	 */
     public $logCategory = 'alipay';
 
-    const PRODUCT_CODE = 'QUICK_MSECURITY_PAY';
+    const APP_PRODUCT_CODE = 'QUICK_MSECURITY_PAY';
 
-    /**
-     * get pay parameter for the client
-     * @var string order id
-     * @var string order subject
-     * @var string amount
-     * @var string body the description of the order, max length is 128
-     * @var string order close timeout
-     * @return string pay string used by client
-     */
-    public function getPayParameter(string $orderId, string $subject, string $amount, string $body, string $timeoutExpress = '30m')
+    const WAP_PRODUCT_CODE = "QUICK_WAP_WAY";
+
+	protected function getAopClient()
+	{
+		$aop = new AopClient();
+
+		$aop->gatewayUrl = $this->gatewayUrl;
+		$aop->appId = $this->appid;
+		if ($this->merchantRsaPrivateKeyFile) {
+			$aop->rsaPrivateKeyFilePath = $this->merchantRsaPrivateKeyFile;
+		} else {
+			$aop->rsaPrivateKey = $this->merchantRsaPrivateKey;
+		}
+		$aop->format = $this->format;
+		$aop->postCharset = $this->charset;
+		$aop->signType = $this->signType;
+		$aop->alipayrsaPublicKey = $this->alipayRsaPublicKey;
+
+		return $aop;
+	}
+
+	/**
+	 * get app pay parameter for the client
+	 * @var string order id
+	 * @var string order subject
+	 * @var string amount
+	 * @var string body the description of the order, max length is 128
+	 * @var string order close timeout
+	 * @return string pay string used by client
+	 */
+	public function getAppPayParameter(string $orderId, string $subject, string $amount, string $body, string $timeoutExpress = '2h')
+	{
+		$aopClient = $this->getAopClient();
+
+		$request = new AlipayTradeAppPayRequest();
+		$bizContent = [
+			'subject' => $subject,
+			'out_trade_no' => $orderId,
+			'body' => $body,
+			'timeout_express' => $timeoutExpress,
+			'total_amount' => $amount,
+			'product_code' => self::APP_PRODUCT_CODE,
+		];
+		$request->setNotifyUrl($this->notifyUrl);
+		$request->setBizContent(json_encode($bizContent));
+		//这里和普通的接口调用不同，使用的是sdkExecute
+		$response = $aopClient->sdkExecute($request);
+
+		Yii::info($response, $this->logCategory);
+		return $response;
+	}
+
+	/**
+	 * get wap pay parameter for the client
+	 * @var string order id
+	 * @var string order subject
+	 * @var string amount
+	 * @var string body the description of the order, max length is 128
+	 * @var string order close timeout
+	 * @return string httpmethod
+	 */
+	public function getWapPayParameter(string $orderId, string $subject, string $amount, string $body, string $httpmethod = 'post')
+	{
+		$aopClient = $this->getAopClient();
+
+		$request = new AlipayTradeWapPayRequest();
+		$bizContent = [
+			'subject' => $subject,
+			'out_trade_no' => $orderId,
+			'body' => $body,
+			'timeout_express' => '2h',
+			'total_amount' => $amount,
+			'product_code' => self::WAP_PRODUCT_CODE,
+		];
+		$request->setNotifyUrl($this->notifyUrl);
+		$request->setBizContent(json_encode($bizContent));
+		$response = $aopClient->pageExecute($request, $httpmethod);
+
+		Yii::info($response, $this->logCategory);
+		return $response;
+	}
+
+	// 兼容上一版本
+    public function getPayParameter(string $orderId, string $subject, string $amount, string $body, string $timeoutExpress = '2h')
     {
-        $aop = new AopClient();
-
-        $aop->gatewayUrl = $this->gatewayUrl;
-        $aop->appId = $this->appid;
-	    if ($this->merchantRsaPrivateKeyFile) {
-		    $aop->rsaPrivateKeyFilePath = $this->merchantRsaPrivateKeyFile;
-	    } else {
-		    $aop->rsaPrivateKey = $this->merchantRsaPrivateKey;
-	    }
-        $aop->format = $this->format;
-        $aop->postCharset = $this->charset;
-        $aop->signType = $this->signType;
-        $aop->alipayrsaPublicKey = $this->alipayRsaPublicKey;
-
-        $request = new AlipayTradeAppPayRequest();
-        $bizContent = [
-            'subject' => $subject,
-            'out_trade_no' => $orderId,
-            'body' => $body,
-            'timeout_express' => $timeoutExpress,
-            'total_amount' => $amount,
-            'product_code' => self::PRODUCT_CODE,
-        ];
-        $request->setNotifyUrl($this->notifyUrl);
-        $request->setBizContent(json_encode($bizContent));
-        //这里和普通的接口调用不同，使用的是sdkExecute
-        $response = $aop->sdkExecute($request);
-
-        Yii::info($response, $this->logCategory);
-        return $response;
+	    return $this->getPayParameter($orderId, $subject, $amount, $body, $timeoutExpress);
     }
 
     /**
